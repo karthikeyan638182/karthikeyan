b8a1aca67bb5b377f179a018333160d56d0fb3d8
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2017-09-12 15:56:07 +0530

    Create evenrnot.c

---
+++ evenrnot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter the value of n");
+scanf("%d",&n);
+if((n%2)==0)
+{
+printf("the number is even");
+}
+else
+{
+printf("the number is odd");
+}
+}
