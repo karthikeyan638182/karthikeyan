e454489a366ae917e01d795e44818a49ed82aa18
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2017-09-12 15:16:07 +0530

    Create testcase.c

---
+++ testcase.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("Enter the value of n");
+scanf("%d",&n);
+if(n>0)
+{
+printf("The number is positive");
+}
+else if(n<0)
+{
+printf("The number is negative");
+}
+else
+{
+printf("the value is zero");
+}
+}
