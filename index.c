ea0716eb83036cd8d01d365979cde192aee64598
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-01-19 07:58:27 +0000

    Update index.c

--- index.c
+++ index.c
@@ -4,9 +4,12 @@ void main()
 int i,a[100],n;
 printf("Enter the value of n");
 scanf("%d",&n);
-for(i=0;i<=n;i++)
+for(i=0;i<n;i++)
 {
-scanf("%d",&a[i]);  
+scanf("%d",&a[i]); 
+}
+for(i=0;i<n;i++)
+{
 printf("%d%d",a[i],i);
 }
 }
