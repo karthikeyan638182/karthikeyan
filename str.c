97c2284a2c732689ee2847316df62f83b4f3e30f
karthikeyan638182 <30829996+karthikeyan638182@users.noreply.github.com>
2018-01-23 16:34:00 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int i,n
+printf("Enter the string");
+scanf("%s",&a);
+printf("Enter tha value of n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%s",a);
+}
+}
